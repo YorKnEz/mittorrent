a0f7652c28e84357bbecf27e46ea89c91baf8a7c
Alex Mitreanu <alexmitreanu04@gmail.com>
2023-12-05 10:48:15 +0200

    logger lib

---
+++ lib/logger.c
@@ -0,0 +1,24 @@
+#include "logger.h"
+
+void print(log_t log_type, const char* fmt, ...) {
+    if (log_type == LOG_DEBUG && ENABLE_DEBUG != 1) {
+        return;
+    }
+
+    va_list args;
+    va_start(args, fmt);
+
+    // if (log_type == LOG_DEBUG) {
+    //     printf("[%d] ", getpid());
+    // }
+
+    vprintf(fmt, args);
+
+    if (log_type == LOG_ERROR) {
+        printf("errno: %s\n", strerror(errno));
+    }
+    
+    fflush(stdout);
+    
+    va_end(args);
+}
---
+++ lib/logger.h
@@ -0,0 +1,15 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <string.h>
+
+#define ENABLE_DEBUG 1
+
+typedef enum { LOG, LOG_ERROR, LOG_DEBUG } log_t;
+
+void print(log_t log_type, const char* fmt, ...);
+
+#endif
