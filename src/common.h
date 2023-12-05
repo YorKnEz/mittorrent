2eb6825bc75bd79095e0c9b4a3a22d9356527325
Alex Mitreanu <alexmitreanu04@gmail.com>
2023-12-05 10:58:04 +0200

    common header

---
+++ src/common.h
@@ -0,0 +1,20 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <string.h>
+
+#include "file.h"
+#include "key.h"
+#include "network.h"
+#include "logger.h"
+#include "dht.h"
+#include "query.h"
+
+#define BOOTSTRAP_SERVER_IP     inet_addr("127.0.0.1")
+#define BOOTSTRAP_SERVER_PORT   12345
+
+#endif
